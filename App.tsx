2975143b89cfbc6b99b481df403b72922e4e2d1a
João Medeiros <jv.medeiros.gallina@gmail.com>
2025-01-30 20:36:23 -0300

    feat: impl theme

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { Groups } from '@screens/Groups';
+import { ThemeProvider } from 'styled-components';
+import theme from '@theme/index';
 
 export default function App() {
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
   );
 }
