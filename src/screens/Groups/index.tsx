0da09b22518ac3d99044a0c45db608a9563d2adb
João Medeiros <jv.medeiros.gallina@gmail.com>
2025-01-30 23:06:59 -0300

    wip: impl header component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,10 @@
-import { Text, View } from 'react-native';
-import { Container, Title } from './styles';
+import { Header } from '@components/Header';
+import { Container } from './styles';
 
 export function Groups() {
   return (
     <Container>
-      <Title>Groups</Title>
+      <Header />
     </Container>
   );
 }
