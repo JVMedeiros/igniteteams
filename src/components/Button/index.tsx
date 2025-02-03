22dd640c1dc0935751f6216fa97cb0e874002de2
João Medeiros <jv.medeiros.gallina@gmail.com>
2025-02-02 22:15:25 -0300

    wip: Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,18 @@
+import { ButtonTypeStyleProps, Container, Title } from "./styles";
+
+
+type ButtonProps = {
+  title: string;
+  type?: ButtonTypeStyleProps
+}
+
+export function Button({title, type = 'PRIMARY', ...rest}: ButtonProps) {
+  return (
+    <Container 
+      type={type} 
+      {...rest}
+    >
+      <Title>{title}</Title>
+    </Container>
+  );
+}
