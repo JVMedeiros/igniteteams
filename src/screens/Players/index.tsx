d954d7c1b8221353f0cd61453228e56572065fe4
João Medeiros <jv.medeiros.gallina@gmail.com>
2025-02-03 21:42:51 -0300

    wip: players screen

---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,20 @@
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { Container } from "./styles";
+
+type PlayersProps = {
+
+}
+
+export function Players() {
+  return (
+    <Container>
+      <Header showBackButton/>
+      <Highlight 
+        title="Nome da turma"
+        subtitle="adicione a galera e separe o time"
+      />
+      
+    </Container>
+  )
+}
